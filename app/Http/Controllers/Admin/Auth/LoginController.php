dc7d8a6e1ed6657effd1458f48663d4625315324
marlonauthority <marlon_authority@msn.com>
2018-06-08 17:23:37 -0400

    Criando login administrativo

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace CodeFlix\Http\Controllers\Auth;
+namespace CodeFlix\Http\Controllers\Admin\Auth;
 
 use CodeFlix\Http\Controllers\Controller;
 use CodeFlix\Models\User;
@@ -44,5 +44,10 @@ class LoginController extends Controller
         $data = $request->only($this->username(), 'password');
         $data['role'] = User::ROLE_ADMIN;
         return $data;
+    }
+
+    public function showLoginForm()
+    {
+        return view('admin.auth.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,17 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group([
     'prefix' => 'admin',
     'as' => 'admin.',
-    'middleware' => 'can:admin'
+    'namespace' => 'Admin\\'
 ], function (){
 
-    Route::get('/', function () {
-        return "Área Administrativa";
-    });
-
+    Route::name('login')->get('login', 'Auth\LoginController@showLoginForm');
+    Route::post('login', 'Auth\LoginController@login');
+        Route::group(['middleware' => 'can:admin'], function(){
+           Route::name('logout')->post('logout', 'Auth\LoginController@logout');
+           Route::get('dashboard', function (){
+               return "Área Administrativa Funcionando";
+           });
+        });
 });
 
 
