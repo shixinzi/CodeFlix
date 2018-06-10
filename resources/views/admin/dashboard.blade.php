a2e4b680292ff80121a4cfb213829f95207baf2e
marlon <marlon_authority@msn.com>
2018-06-10 14:56:51 -0400

    recuperar senha

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        Àrea administrativa funcionando
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
@@ -31,7 +36,7 @@ Route::group([
         Route::group(['middleware' => 'can:admin'], function(){
            Route::name('logout')->post('logout', 'Auth\LoginController@logout');
            Route::get('dashboard', function (){
-               return "Área Administrativa Funcionando";
+               return view('admin.dashboard');
            });
         });
 });
