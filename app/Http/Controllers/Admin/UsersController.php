f8efbb2235d747bf890f7eaf6d6c38debaac7a85
marlonauthority <marlon_authority@msn.com>
2018-06-12 11:16:47 -0400

    validacao do form

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -50,7 +50,10 @@ class UsersController extends Controller
         $form = \FormBuilder::create(UserForm::class);
 
         if (!$form->isValid()){
-            //
+            return redirect()
+                ->back()
+                ->withErrors($form->getErrors())
+                ->withInput();
         }
         $data = $form->getFieldValues();
         $data['role'] = User::ROLE_ADMIN;
