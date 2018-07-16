d90728692258b0a5cb4a3d3381fced4f94320fd2
marlonauthority <marlon_authority@msn.com>
2018-07-16 15:19:52 -0400

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,22 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 \ApiRoute::version('v1',function (){
 
+
+
     ApiRoute::group(['namespace'=> 'CodeFlix\Http\Controllers\Api', 'as' => 'api'], function ()
     {
-        ApiRoute::post('/access_token', 'AuthController@accessToken')
-                    ->name('.access_token');
+        ApiRoute::post('/access_token',
+            [
+            'uses' => 'AuthController@accessToken',
+                'middleware' => 'api.throttle',
+                'limit' => 10,
+                'expires' => 1
+            ])->name('.access_token');
+
+        ApiRoute::group(['middleware' => 'api.throttle', 'limit' => 100, 'expires' => 3], function()
+        {
+
+        });
     });
 
 });
